bd8bc420f93ff6615451123fa5cb619df77b959d
df40420 <Danushka.Fernando@aepona.com>
2020-03-05 17:49:06 +0530

    Fixing context finising bug

--- Delete_Old_Logs/index.js
+++ Delete_Old_Logs/index.js
@@ -1,84 +1,116 @@
 var querystring = require('querystring');
-var http = require('https');
+var https = require('https');
 
-exports.handler = function(event, context) {
+var host = "search-xxxx.xxx.es.amazonaws.com";
 
-    var host = "search-xxx.xxx.es.amazonaws.com";
-    var indices = [];
-    var get_indices_options = {
-        host: host,
-        port: 443,
-        path: '/*',
-        method: 'GET'
-    };
-    var indicesOutput = '';
+exports.handler = async (event, context) => {
 
-    var get_indices_request = http.request(get_indices_options, function(res) {
-        res.setEncoding('utf8');
-        res.on('data', (chunk) => {
-            indicesOutput += chunk;
-        });
+    var indices = await getIndices(context);
 
-        res.on('end', () => {
-            console.log('Get Indices Response: ');
-            indices = JSON.parse(indicesOutput);
-            for (let key in indices) {
-                if (!key.startsWith("development_")) {
-                    continue;
+    for (let key in indices) {
+        if (!key.startsWith("development_")) {
+            continue;
+        }
+        if (indices.hasOwnProperty(key)) {
+            try {
+                var createdTimeStamp = indices[key].settings.index.creation_date;
+                var createdDate = new Date(parseInt(createdTimeStamp));
+                console.log("Current Index : " + key + ", Created Date : " + createdDate);
+                var now = new Date();
+                var ageDays = (now.getTime() - createdDate.getTime()) / (1000 * 3600 * 24);
+                console.log("Current Index : " + key + ", Age in Days : " + ageDays);
+                if (ageDays > 3) {
+                    console.log("Deleting the index : " + key);
+                    await delIndices(context, key);
                 }
-                if (indices.hasOwnProperty(key)) {
-                    try {
-                        var createdTimeStamp = indices[key].settings.index.creation_date;
-                        var createdDate = new Date(parseInt(createdTimeStamp));
-                        console.log("Current Index : " + key + ", Created Date : " + createdDate);
-                        var now = new Date();
-                        var ageDays = (now.getTime() - createdDate.getTime()) / (1000 * 3600 * 24);
-                        console.log("Current Index : " + key + ", Age in Days : " + ageDays);
-                        if (ageDays > 3) {
-                            console.log("Deleting the index : ");
-                            var delete_index_options = {
-                                host: host,
-                                port: 443,
-                                path: '/' + key,
-                                method: 'DELETE',
-                                headers: {
-                                    'Content-Type': 'application/json',
-                                    'Content-Length': Buffer.byteLength(JSON.stringify(alias))
-                                }
-                            };
-                            var deleteOutput = '';
-                            var delete_index_req = http.request(delete_index_options, function(delete_res) {
-                                delete_res.setEncoding('utf8');
-                                delete_res.on('data', (chunk) => {
-                                    deleteOutput += chunk;
-                                });
-
-                                delete_res.on('end', () => {
-                                    console.log('Delete Index Response: ' + deleteOutput);
-                                    context.succeed();
-                                });
-                                delete_res.on('error', function(e) {
-                                    console.log("Delete Index Got error: " + e.message);
-                                    context.done(null, 'FAILURE');
-                                });
-
-                            });
-                            delete_index_req.end();
-                        }
-                    } catch (e) {
-                      console.log(e);
-                      // Continue when timestamp is not found
-                    }
-                }
+            } catch (e) {
+                console.log(e);
+                // Continue when timestamp is not found
             }
-            context.succeed();
-        });
-        res.on('error', function(e) {
-            console.log("Get Indices Got error: " + e.message);
-            context.done(null, 'FAILURE');
-            return;
-        });
-    });
-    get_indices_request.end();
+        }
+    }
+    context.succeed();
+}
 
+function getIndicesPromise(context) {
+    return new Promise((resolve, reject) => {
+        var aliases = {};
+        var indices = [];
+        var get_indices_options = {
+            host: host,
+            port: 443,
+            path: '/*',
+            method: 'GET'
+        };
+        var indicesOutput = '';
+
+        var get_indices_request = https.request(get_indices_options, function(res) {
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                indicesOutput += chunk;
+            });
+            res.on('end', () => {
+                console.log('Get Indices Response: ');
+                indices = JSON.parse(indicesOutput);
+                resolve(indices);
+            });
+            res.on('error', function(e) {
+                console.log("Get Indices Got error: " + e.message);
+                reject(e);
+            });
+        });
+        get_indices_request.end();
+    });
+}
+
+async function getIndices(context) {
+    try {
+        let http_promise = getIndicesPromise(context);
+        let indices = await http_promise;
+        return indices;
+    } catch (error) {
+        console.log(error);
+        return "";
+    }
+}
+
+function delIndicesPromise(context, key) {
+    return new Promise((resolve, reject) => {
+        var delete_index_options = {
+            host: host,
+            port: 443,
+            path: '/' + key,
+            method: 'DELETE'
+        };
+        var deleteOutput = '';
+        var delete_index_req = https.request(delete_index_options, function(delete_res) {
+            delete_res.setEncoding('utf8');
+            delete_res.on('data', (chunk) => {
+                console.log('Data: ' + chunk);
+                deleteOutput += chunk;
+            });
+
+            delete_res.on('end', () => {
+                console.log('Delete Index Response: ' + deleteOutput);
+                resolve(deleteOutput);
+            });
+            delete_res.on('error', function(e) {
+                console.log("Delete Index Got error: " + e.message);
+                reject(e);
+            });
+
+        });
+        delete_index_req.end();
+    });
+}
+
+async function delIndices(context, key) {
+    try {
+        let http_promise = delIndicesPromise(context, key);
+        let del_indices = await http_promise;
+        return del_indices;
+    } catch (error) {
+        console.log(error);
+        return "";
+    }
 }
--- Rollover/index.js
+++ Rollover/index.js
@@ -1,149 +1,172 @@
 var querystring = require('querystring');
 var http = require('https');
+var host = "search-xxx.xxx.es.amazonaws.com";
 
-exports.handler = function(event, context) {
-
-    var obj = {
-        "conditions": {
-            "max_age": "1d"
+exports.handler = async (event, context) => {
+    var indices = await getIndices(context);
+    for (let key in indices) {
+        if (key.startsWith(".")) {
+            continue;
         }
-    };
-    var host = "search-xxx.xxx.es.amazonaws.com";
-    var indices = [];
-
-    var get_indices_options = {
-        host: host,
-        port: 443,
-        path: '/*',
-        method: 'GET'
-    };
-
-    var indicesOutput = '';
-
-
-    var get_indices_request = http.request(get_indices_options, function(res) {
-        res.setEncoding('utf8');
-        res.on('data', (chunk) => {
-            indicesOutput += chunk;
-        });
-
-        res.on('end', () => {
-            console.log('Get Indices Response: ');
-            indices = JSON.parse(indicesOutput);
-            for (let key in indices) {
-                if (indices.hasOwnProperty(key)) {
-                    var index = indices[key];
-                    var aliasesOutput = '';
-                    var rolloverOutput = '';
-                    var aliasName = key.substr(0, key.lastIndexOf("-"));
-                    if (!Object.keys(index.aliases).length) {
-                        console.log("Creating Alias for : " + key + ", Alias name : " + aliasName);
-                        var alias = {
-                            "actions": [{
-                                "add": {
-                                    "index": key,
-                                    "alias": aliasName,
-                                    "is_write_index": true
-                                }
-                            }]
-                        };
-
-                        var post_alias_options = {
-                            host: host,
-                            port: 443,
-                            path: '/_aliases',
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'Content-Length': Buffer.byteLength(JSON.stringify(alias))
-                            }
-                        };
-                        var post_alias_req = http.request(post_alias_options, function(post_alias_res) {
-                            post_alias_res.setEncoding('utf8');
-                            post_alias_res.on('data', (chunk) => {
-                                aliasesOutput += chunk;
-                            });
-
-                            post_alias_res.on('end', () => {
-                                console.log('Post Alias Response: ' + aliasesOutput);
-                                var post_rollover_options = {
-                                    host: host,
-                                    port: 443,
-                                    path: '/' + aliasName + '/_rollover',
-                                    method: 'POST',
-                                    headers: {
-                                        'Content-Type': 'application/json',
-                                        'Content-Length': Buffer.byteLength(JSON.stringify(obj))
-                                    }
-                                };
-                                var post_rollover_req = http.request(post_rollover_options, function(post_rollover_res) {
-                                    post_rollover_res.setEncoding('utf8');
-                                    post_rollover_res.on('data', (chunk) => {
-                                        rolloverOutput += chunk;
-                                    });
-
-                                    post_rollover_res.on('end', () => {
-                                        console.log('Post Rollover Response: ' + rolloverOutput);
-                                        context.succeed();
-                                    });
-                                    post_rollover_res.on('error', function(e) {
-                                        console.log("Post Rollover Got error: " + e.message);
-                                        context.done(null, 'FAILURE');
-                                    });
-
-                                });
-                                post_rollover_req.write(JSON.stringify(obj));
-                                post_rollover_req.end();
-                                context.succeed();
-                            });
-                            post_alias_res.on('error', function(e) {
-                                console.log("Post Alias Got error: " + e.message);
-                                context.done(null, 'FAILURE');
-                            });
-
-                        });
-                        post_alias_req.write(JSON.stringify(alias));
-                        post_alias_req.end();
-                    } else {
-                        console.log("Starting to Rollover Index : " + key);
-                        var post_rollover_options = {
-                            host: host,
-                            port: 443,
-                            path: '/' + aliasName + '/_rollover',
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'Content-Length': Buffer.byteLength(JSON.stringify(obj))
-                            }
-                        };
-                        var post_rollover_req = http.request(post_rollover_options, function(post_rollover_res) {
-                            post_rollover_res.setEncoding('utf8');
-                            post_rollover_res.on('data', (chunk) => {
-                                rolloverOutput += chunk;
-                            });
-
-                            post_rollover_res.on('end', () => {
-                                console.log('Post Rollover Response: ' + rolloverOutput);
-                                context.succeed();
-                            });
-                            post_rollover_res.on('error', function(e) {
-                                console.log("Post Rollover Got error: " + e.message);
-                                context.done(null, 'FAILURE');
-                            });
-
-                        });
-                        post_rollover_req.write(JSON.stringify(obj));
-                        post_rollover_req.end();
-                    }
-                }
+        if (indices.hasOwnProperty(key)) {
+            var index = indices[key];
+            var aliasName = key.substr(0, key.lastIndexOf("-"));
+            if (!Object.keys(index.aliases).length) {
+                console.log("Creating Alias for : " + key + ", Alias name : " + aliasName);
+                var alias = await createAlias(context, aliasName, key);
             }
-            context.succeed();
+            await rollover(context, aliasName, key);
+        }
+    }
+    context.succeed();
+}
+
+function getIndicesPromise(context) {
+    return new Promise((resolve, reject) => {
+        var aliases = {};
+        var indices = [];
+        var get_indices_options = {
+            host: host,
+            port: 443,
+            path: '/*',
+            method: 'GET'
+        };
+        var indicesOutput = '';
+
+        var get_indices_request = https.request(get_indices_options, function(res) {
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                indicesOutput += chunk;
+            });
+            res.on('end', () => {
+                console.log('Get Indices Response: ');
+                indices = JSON.parse(indicesOutput);
+                resolve(indices);
+            });
+            res.on('error', function(e) {
+                console.log("Get Indices Got error: " + e.message);
+                reject(e);
+            });
         });
-        res.on('error', function(e) {
-            console.log("Get Indices Got error: " + e.message);
-            context.done(null, 'FAILURE');
-            return;
-        });
+        get_indices_request.end();
     });
-    get_indices_request.end();
+}
+
+async function getIndices(context) {
+    try {
+        let http_promise = getIndicesPromise(context);
+        let indices = await http_promise;
+        return indices;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+}
+
+function createAliasPromise(context, aliasName, index) {
+    return new Promise((resolve, reject) => {
+        var alias = {
+            "actions": [{
+                "add": {
+                    "index": index,
+                    "alias": aliasName,
+                    "is_write_index": true
+                }
+            }]
+        };
+        var post_alias_options = {
+            host: host,
+            port: 443,
+            path: '/_aliases',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(JSON.stringify(alias))
+            }
+        };
+        var aliasesOutput = '';
+
+        var post_alias_req = https.request(post_alias_options, function(post_alias_res) {
+            post_alias_res.setEncoding('utf8');
+            post_alias_res.on('data', (chunk) => {
+                console.log("Data : " + chunk);
+                aliasesOutput += chunk;
+            });
+
+            post_alias_res.on('end', () => {
+                console.log('Post Alias Response: ' + aliasesOutput);
+                resolve(aliasesOutput);
+            });
+            post_alias_res.on('error', function(e) {
+                console.log("Post Alias Got error: " + e.message);
+                reject(e);
+            });
+
+        });
+        post_alias_req.write(JSON.stringify(alias));
+        post_alias_req.end();
+    });
+}
+
+async function createAlias(context, aliasName, index) {
+    try {
+        let http_promise = createAliasPromise(context, aliasName, index);
+        let alias_output = await http_promise;
+        return alias_output;
+    } catch (error) {
+        console.log(error);
+        return "";
+    }
+}
+
+function rolloverPromise(context, aliasName, index) {
+    return new Promise((resolve, reject) => {
+        var obj = {
+            "conditions": {
+                "max_age": "1d"
+            }
+        };
+        var rolloverOutput = '';
+        var post_rollover_options = {
+            host: host,
+            port: 443,
+            path: '/' + aliasName + '/_rollover',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Length': Buffer.byteLength(JSON.stringify(obj))
+            }
+        };
+        var post_rollover_req = https.request(post_rollover_options, function(post_rollover_res) {
+            post_rollover_res.setEncoding('utf8');
+            post_rollover_res.on('data', (chunk) => {
+                console.log("Data : " + chunk);
+                rolloverOutput += chunk;
+            });
+
+            post_rollover_res.on('end', () => {
+                console.log('Post Rollover Response: ' + rolloverOutput);
+                resolve(rolloverOutput);
+            });
+            post_rollover_res.on('error', function(e) {
+                console.log("Post Rollover Got error: " + e.message);
+                reject(e);
+            });
+
+        });
+        console.log("Starting to Rollover Index : " + index + ", Alias : " + aliasName);
+        post_rollover_req.write(JSON.stringify(obj));
+        post_rollover_req.end();
+    });
+}
+
+async function rollover(context, aliasName, index) {
+    try {
+        let http_promise = rolloverPromise(context, aliasName, index);
+        let rollover_output = await http_promise;
+        return rollover_output;
+    } catch (error) {
+        console.log(error);
+        return "";
+    }
 }
